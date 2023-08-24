4e40f4dcd448990cc805aa3f6e65bcd08800f63a
tais <tais15oliva@gmail.com>
2023-08-24 16:46:24 -0300

    API REST of Posts

--- src/medias/dto/update-media.dto.ts
+++ src/medias/dto/update-media.dto.ts
@@ -1,14 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMediaDto } from './create-media.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateMediaDto {
 
-    @IsString()
-    @IsNotEmpty()
-    title: string
-    
-    @IsString()
-    @IsNotEmpty()
-    username: string
-}
+export class UpdateMediaDto extends PartialType(CreateMediaDto) {}
--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -2,9 +2,11 @@ export class Post {
 
     private _title: string
     private _text: string
+    private _image:string
 
-    constructor(_title: string, _text:string){
+    constructor(_title: string, _text:string, _image:string){
         this._title = _title
         this._text = _text
+        this._image = _image
     }
 }
